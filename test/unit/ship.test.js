af62f507b61b377bbac69d6c48588170ebc4ec2b
Rehmsy <sarahrehmer@gmail.com>
2018-07-27 16:26:36 -0700

    ship min of 0 test passing

--- test/unit/ship.test.js
+++ test/unit/ship.test.js
@@ -23,10 +23,36 @@ describe('Ship model', () => {
         assert.isUndefined(ship.validateSync());
     });
 
-    it('name, oxygen, lifeSupport, fuel, hal, payload, hull all required', () => {
+    it('validates all required fields', () => {
         const ship = new Ship({});
         const errors = getErrors(ship.validateSync(), 7);
         assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.oxygen.kind, 'required');
+        assert.equal(errors.lifeSupport.kind, 'required');
+        assert.equal(errors.fuel.kind, 'required');
+        assert.equal(errors.hal.kind, 'required');
+        assert.equal(errors.payload.kind, 'required');
+        assert.equal(errors.hull.kind, 'required');
     }); 
+
+    it('validates fields that have min of 0', () => {
+        const ship = new Ship({
+            name: 'Shape Shipster',
+            oxygen: -1,
+            lifeSupport: -85, 
+            fuel: -100,
+            hal: -60,
+            payload: -95,
+            hull: -60
+        });
+
+        const errors = getErrors(ship.validateSync(), 6);
+        assert.equal(errors.oxygen.kind, 'min');
+        assert.equal(errors.lifeSupport.kind, 'min');
+        assert.equal(errors.fuel.kind, 'min');
+        assert.equal(errors.hal.kind, 'min');
+        assert.equal(errors.payload.kind, 'min');
+        assert.equal(errors.hull.kind, 'min');
+    });
 
 });
