0a1958839b60141cf9175d5be7befccc07bfad3e
Mark Myers <markalope@gmail.com>
2018-07-27 14:47:29 -0700

    built out server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+/* eslint no-console: off */
+require('dotenv').config();
+const connect = require('./lib/util/connect');
+const { createServer } = require('http');
+const app = require('./lib/app');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/halchemy';
+
+const PORT = process.env.PORT || 3000;
+
+const server = createServer(app);
+server.listen(PORT, () => {
+    console.log('HAL is actively running on', server.address().port);
+});
