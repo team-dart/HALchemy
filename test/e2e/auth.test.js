73ccae6955eeae3471d6a5101ae04e267e82be9d
Mark Myers <markalope@gmail.com>
2018-07-27 15:05:56 -0700

    added inquirer and additional scaffolding files

---
+++ test/e2e/auth.test.js
@@ -0,0 +1,89 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+const { checkOk } = request;
+
+describe('Auth API', () => {
+
+    beforeEach(() => dropCollection('users'));
+
+    let token;
+    beforeEach(() => {
+        return request
+            .post('/api/auth/signup')
+            .send({
+                email: 'me@me.com',
+                password: 'abc'
+            })
+            .then(checkOk)
+            .then(({ body }) => {
+                token = body.token;
+            });
+    });
+
+    it('signs up a user', () => {
+        assert.isDefined(token);
+    });
+
+
+    it('verifies a token', () => {
+        return request
+            .get('/api/auth/verify')
+            .set('Authorization', token)
+            .then(checkOk);
+    });
+
+    it('can sign in a user', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: 'abc'
+            })
+            .then(checkOk)
+            .then(({ body }) => {
+                assert.isDefined(body.token);
+            });
+    });
+
+    it('fails on wrong password', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'me@me.com',
+                password: 'bad'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
+            });
+    });
+
+    it('cannot signup with same email', () => {
+        return request
+            .post('/api/auth/signup')
+            .send({
+                email: 'me@me.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 400);
+                assert.equal(res.body.error, 'Email already in use');
+            });        
+    });
+
+    it('Gives 401 on bad email signin', () => {
+        return request
+            .post('/api/auth/signin')
+            .send({
+                email: 'bad@me.com',
+                password: 'abc'
+            })
+            .then(res => {
+                assert.equal(res.status, 401);
+                assert.equal(res.body.error, 'Invalid email or password');
+            });
+    });
+    
+});
